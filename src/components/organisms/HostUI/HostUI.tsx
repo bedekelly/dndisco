81a104e3c74b82f27de1deac7bde7eba1b1c8297
Bede Kelly <bede.kelly@alpima.com>
2021-07-18 16:37:17 +0100

    Remove extraneous import

--- src/components/organisms/HostUI/HostUI.tsx
+++ src/components/organisms/HostUI/HostUI.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 
 import UnlockAudio from "../../../audio/UnlockAudio";
 import { useBuffers } from "../../../audio/useBuffers";
