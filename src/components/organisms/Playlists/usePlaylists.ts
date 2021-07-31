f0137958134caf20488f71e554a5c1d2f0ca9c07
Bede Kelly <bede.kelly@alpima.com>
2021-07-31 23:11:27 +0100

    Extract out the loading-counter logic to fix a bug with hot reloading

--- src/components/organisms/Playlists/usePlaylists.ts
+++ src/components/organisms/Playlists/usePlaylists.ts
@@ -1,40 +1,54 @@
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect, useCallback, useRef } from "react";
 import globalSocket from "../../../network/globalSocket";
 
 type PlaylistID = string;
+
+function useLoadingCounter() {
+  const loadingCount = useRef(0);
+
+  const push = useCallback(() => {
+    loadingCount.current = loadingCount.current + 1;
+  }, []);
+  const pop = useCallback(() => {
+    loadingCount.current = loadingCount.current + -1;
+  }, []);
+
+  useEffect(() => {
+    loadingCount.current = 0;
+    return () => {
+      loadingCount.current = 0;
+    };
+  }, []);
+
+  return { count: loadingCount.current, push, pop };
+}
 
 /**
  * Loads the list of playlist IDs from the server.
  * Returns a callback to create a new playlist.
  */
 export default function usePlaylists() {
-  const [loadingCount, setLoadingCount] = useState(0);
+  const { count, push, pop } = useLoadingCounter();
   const [playlists, setPlaylists] = useState<PlaylistID[]>([]);
 
   useEffect(() => {
-    setLoadingCount((count) => count + 1);
+    push();
     globalSocket.emit("getPlaylists", (playlists: PlaylistID[]) => {
-      setLoadingCount((count) => count - 1);
+      pop();
       return setPlaylists(playlists);
     });
-  }, []);
+  }, [pop, push]);
 
   const createPlaylist = useCallback(() => {
-    setLoadingCount((count) => count + 1);
+    push();
     return new Promise<PlaylistID>((resolve) => {
       globalSocket.emit("createPlaylist", (playlistID: PlaylistID) => {
         setPlaylists((playlists) => [...playlists, playlistID]);
-        setLoadingCount((count) => count - 1);
+        pop();
         resolve(playlistID);
       });
     });
-  }, []);
+  }, [pop, push]);
 
-  useEffect(() => {
-    return () => {
-      setLoadingCount(0);
-    };
-  }, []);
-
-  return { playlists, createPlaylist, loading: loadingCount !== 0 };
+  return { playlists, createPlaylist, loading: count !== 0 };
 }
